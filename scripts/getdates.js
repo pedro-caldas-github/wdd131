eaf3b060b94f515257a24a4e69d3eec580c6b05d
pedro-caldas-github <josepedro.caldas@gmail.com>
2025-11-05 20:52:10 -0300

    fixxing 2

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,20 +1,14 @@
 const today = new Date();
-
 const currentYear = today.getFullYear();
-
 const copyrightElement = document.getElementById("copyright-year");
-
-if (copyrightElement) {
-    copyrightElement.textContent = copyrightElement.textContent.replace("2025", currentYear);
-} else {
-    console.error("Element with ID 'copyright-year' not found.");
-}
-
-
 const lastModifiedElement = document.getElementById("lastModified");
 
+if (copyrightElement) {
+    // Insere o ano atual no SPAN do copyright.
+    copyrightElement.textContent = currentYear;
+}
+
 if (lastModifiedElement) {
+    // Insere a data da última modificação.
     lastModifiedElement.textContent = document.lastModified;
-} else {
-    console.error("Element with ID 'lastModified' not found.");
 }
